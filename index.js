e3d7a1f05b68eadbd7fb00048f0a7190845d20ad
PhanVu5 <96001569+PhanVu5@users.noreply.github.com>
2023-10-01 13:50:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-const inputs = document.querySelectorAll(".input");
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Animated Login Form</title>
 
-
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
-}
-
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
-}
-
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
-});
-
-// Check-email
-
-// passport.use(new MailruStrategy({
-//     clientID: MAIL_APP_ID,
-//     clientSecret: MAIL_APP_SECRET,
-//     callbackURL: "http://localhost:3000/auth/mail/callback"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     User.findOrCreate({ mailId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="./style.css">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<img class="wave" src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/wave.png">
+	<div class="container">
+		<div class="img">
+			<img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/bg.svg">
+		</div>
+		<div class="login-content">
+			<form action="/login" method="post">
+				<img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/avatar.svg">
+				<h2 class="title">Welcome</h2>
+				<div class="input-div one" id="frame_account">
+					<div class="i">
+						<i class="fas fa-user" id="tk"></i>
+					</div>
+					<div class="div">
+						<h5>Username</h5>
+						<input type="email" id="email" class="input" required>
+						<p id="email-error" class="error-message" style="position: absolute;
+    						font-size: 10px;
+    						bottom: -28px;
+    						color: red;"></p>
+					</div>
+				</div>
+				<div class="input-div pass">
+					<div class="i">
+						<i class="fas fa-lock"></i>
+					</div>
+					<div class="div">
+						<h5>Password</h5>
+						<input type="password" class="input" required>
+					</div>
+				</div>
+				<a href="#">Forgot Password?</a>
+				<input type="submit" class="btn" value="Login">
+			</form>
+		</div>
+	</div>
+	<script src="./index.js"></script>
+	<script src="./CheckAccount.js"></script>
+</body>
+</html>
